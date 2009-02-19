bb7caad4b7768447bc8f63e93f8ae090178b8e02
ldm314 <Brian@.(none)>
2009-02-18 19:16:04 -0800

    Clean up files that shouldn't have been checked in, fix manifest

--- rhodes/rhodes-framework/pkg/rhodes-framework-0.2.6/lib/rhodes-framework.rb
+++
@@ -1,2 +0,0 @@
-require 'version.rb'
-$rhodeslib = File.dirname(__FILE__)
--- rhodes/rhodes-generator/pkg/rhodes-generator-0.2.6/generators/templates/application/config.rb
+++
@@ -1,7 +0,0 @@
-require 'rho'
-
-Rho::RhoConfig.start_path = '/app'
-
-Rho::RhoConfig.rhobundle_zip_url = nil
-
-Rho::RhoConfig.rhobundle_zip_pwd = nil
