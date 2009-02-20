25352154fc1072a7b87b455f578bcce88e00d3c3
lars <larsburgess@gmail.com>
2009-02-19 23:26:02 -0800

    adding stack traces

--- rhodes/rhodes-framework/lib/rho/rho.rb
+++ rhodes/rhodes-framework/lib/rho/rho.rb
@@ -84,7 +84,7 @@ module Rho
         get_app(req['application']).send :serve, req, res
         return send_response(res)
       rescue Exception => e
-        return send_error(e.message)
+        return send_error(e)
       end	
     end
 
@@ -95,7 +95,7 @@ module Rho
 			get_app(req['application']).send :serve, req, res
 			return send_response_hash(res)
 		rescue Exception => e
-			return send_error(e.message,500,true)
+			return send_error(e,500,true)
 		end	
     end
 	
@@ -106,7 +106,7 @@ module Rho
 			res['request-body'] = RhoController::renderfile(index_name)
 			return send_response(res)
 		rescue Exception => e
-			return send_error(e.message)
+			return send_error(e)
 		end
 	end
 
@@ -166,7 +166,7 @@ module Rho
 		resp
     end
 	
-    def send_error(msg="",status=500,hash=false)
+    def send_error(exception=nil,status=500,hash=false)
       body=''
       body << <<-_HTML_STRING_
 		<html>
@@ -177,7 +177,8 @@ module Rho
 			<body>
 				<p>
       _HTML_STRING_
-      body << 'Error message: ' << msg		
+      body << 'Error: ' << exception.message << "<br/>" if exception
+      body << 'Trace: ' << exception.backtrace.join("\n") if exception
       body << <<-_HTML_STRING_
 				</p>	
 			</body>
